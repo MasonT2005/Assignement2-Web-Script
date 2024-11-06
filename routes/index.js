cae23388d19219ae7d2145f67abe0bbfdcbd1e9f
MasonT2005 <mason.tran@ontariotechu.net>
2024-11-05 19:57:59 -0500

    commit 4

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET index page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Index' });
-});
+
+
+
 /* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home Page' });
 });
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
 /* GET service page. */
 router.get('/service', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
